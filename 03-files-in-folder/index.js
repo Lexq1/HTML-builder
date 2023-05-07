5966c8072665c820c760cd8bb084f752c6f6e8a1
Lexq1 <dedmaksq@gmail.com>
2023-05-07 21:07:02 +0300

    feat: add solution 03-files-in-folder(Вс мая  7 21:07:02 EEST 2023)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const route = path.join(__dirname, 'secret-folder');
+
+fs.readdir(route,{ withFileTypes: true },(err, files) => {
+    if(err) console.log(err);
+    else{
+    	files.forEach(file => {
+    		let route2 = path.join(__dirname, 'secret-folder', file['name']);
+    		let fileName = file.name.slice(0,file.name.indexOf('.'));
+    		let fileSrc = file.name.slice(file.name.indexOf('.') + 1);
+    		let fileSize = 0;
+    		fs.stat(route2,(err,stats) =>{
+    			fileSize += stats['size'] / 1024;
+    			if (!err) console.log(`${fileName} - ${fileSrc} - ${fileSize}kB`);
+    			else console.log(err.message);
+    		})
+    	});	
+    }
+});
