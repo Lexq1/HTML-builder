f3b1e1652019594772edade9dd4c8212ccae63cc
Александр Ступак <76948335+Lexq1@users.noreply.github.com>
2023-05-09 13:03:47 +0300

    feat: add solution 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const route = path.join(__dirname, 'styles');
+const writableRoute = path.join(__dirname,'project-dist');
+const fileWritable = path.join(writableRoute, 'bundle.css');
+const writableStream = fs.createWriteStream(writableRoute, 'utf-8');
+
+fs.readdir(route, { withFileTypes: true }, (err,files) => {
+	if(err) console.log(err);
+	files.forEach(file => {
+		let redableRouteFile = path.join(route,file.name);
+		if(file.isFile() && path.extname(redableRouteFile) === 'css'){
+			const redableStream = fs.createReadStream(redableRouteFile, 'utf-8');
+			redableStream.pipe(writableStream);
+		}
+	})
+})
