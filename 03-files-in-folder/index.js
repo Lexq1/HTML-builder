d8e2e39b14e2ca431d2d4ff1a5055d3b2827b5b7
Lexq1 <dedmaksq@gmail.com>
2023-05-10 15:36:07 +0300

    fix: add function isFile to solution 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,15 +6,17 @@ fs.readdir(route,{ withFileTypes: true },(err, files) => {
     if(err) console.log(err.message);
     else{
     	files.forEach(file => {
-    		let route2 = path.join(__dirname, 'secret-folder', file['name']);
-    		let fileName = file.name.slice(0,file.name.indexOf('.'));
-    		let fileSrc = file.name.slice(file.name.indexOf('.') + 1);
-    		let fileSize = 0;
-    		fs.stat(route2,(err,stats) =>{
-    			fileSize += stats['size'] / 1024;
-    			if (!err) console.log(`${fileName} - ${fileSrc} - ${fileSize}kB`);
-    			else console.log(err.message);
-    		})
+            if (file.isFile()) {
+                let route2 = path.join(__dirname, 'secret-folder', file['name']);
+                let fileName = file.name.slice(0,file.name.indexOf('.'));
+                let fileSrc = file.name.slice(file.name.indexOf('.') + 1);
+                let fileSize = 0;
+                fs.stat(route2,(err,stats) =>{
+                    fileSize += stats['size'] / 1024;
+                    if (!err) console.log(`${fileName} - ${fileSrc} - ${fileSize}kB`);
+                    else console.log(err.message);
+                })  
+            }
     	});	
     }
 });
