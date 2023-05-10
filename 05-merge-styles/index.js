9cd72d9965cf84ad1e564ffb5dd8a5a5bf785d52
Lexq1 <dedmaksq@gmail.com>
2023-05-10 15:45:53 +0300

    fix: fix the error in the solution05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,13 +3,13 @@ const path = require('path');
 const route = path.join(__dirname, 'styles');
 const writableRoute = path.join(__dirname,'project-dist');
 const fileWritable = path.join(writableRoute, 'bundle.css');
-const writableStream = fs.createWriteStream(writableRoute, 'utf-8');
+const writableStream = fs.createWriteStream(fileWritable, 'utf-8');
 
 fs.readdir(route, { withFileTypes: true }, (err,files) => {
 	if(err) console.log(err);
 	files.forEach(file => {
 		let redableRouteFile = path.join(route,file.name);
-		if(file.isFile() && path.extname(redableRouteFile) === 'css'){
+		if(file.isFile() && path.extname(redableRouteFile) === '.css'){
 			const redableStream = fs.createReadStream(redableRouteFile, 'utf-8');
 			redableStream.pipe(writableStream);
 		}
